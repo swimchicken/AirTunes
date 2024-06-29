91a5665c4de9ce651919c915de94caae6fc55538
swimchicken <swimchickenouo@gmail.com>
2024-06-29 20:48:55 +0800

    Initial Commit

---
+++ airtune2/airtune2App.swift
@@ -0,0 +1,17 @@
+//
+//  airtune2App.swift
+//  airtune2
+//
+//  Created by swimchichen on 2024/6/29.
+//
+
+import SwiftUI
+
+@main
+struct airtune2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
